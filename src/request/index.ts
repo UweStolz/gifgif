caf421c5fab1d1b35d81ea025e6322993f252623
UweStolz <uwe.stolz@protonmail.com>
2020-01-31 20:47:04 +0100

    ref: separate helper functions

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import { arrayBufferToBase64 } from './imageHelper';
 
 const apiKey = 'IT2EfZQf0HqGmKIlg0Ts9p7akXdKSBl7';
 
@@ -7,11 +8,18 @@ interface gifResult {
   listSize: number;
 }
 
-export default async function getTrendingGif(index?: number): Promise<gifResult> {
-  const res = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`);
+export async function getGifAsBuffer(imageUrl: string): Promise<string> {
+  const response = await fetch(imageUrl);
+  const arrayBuffer = await response.arrayBuffer();
+  const base64Image = arrayBufferToBase64(arrayBuffer);
+  return base64Image;
+}
+
+export async function getTrendingGif(index?: number): Promise<gifResult> {
+  const response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`);
   let gifObject;
   let originalUrl: string|null = null;
-  const responseJson = await res.json();
+  const responseJson = await response.json();
   const { data } = responseJson;
   const listSize: number = Object.keys(data).length;
 
