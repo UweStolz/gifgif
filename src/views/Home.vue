ac4c2549774a3255a4741383bf62a1a6c0358c8c
UweStolz <uwe.stolz@protonmail.com>
2020-02-01 10:10:42 +0100

    ref: remove boilerplate code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,5 @@
-<template>
-  <div class="home">
-    <img
-      alt="Vue logo"
-      src="../assets/logo.png"
-    >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-// @ is an alias to /src
 import Frontpage from '@/components/Frontpage.vue';
 
  @Component({
