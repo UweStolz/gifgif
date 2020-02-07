5e259f7f1bea3493eff79202c16bb8d4fbcaaf55
UweStolz <uwe.stolz@prodexa.com>
2020-02-07 13:08:53 +0100

    fix: supress eslint message

--- src/util/imageHelper.ts
+++ src/util/imageHelper.ts
@@ -3,6 +3,7 @@ export function imageToBase64Image(imageElement: HTMLImageElement) {
   const canvas = document.createElement('canvas');
   canvas.width = imageElement.width;
   canvas.height = imageElement.height;
+  // eslint-disable-next-line no-unused-expressions
   canvas.getContext('2d')?.drawImage(imageElement, 0, 0);
   const dataURL = canvas.toDataURL('image/webp');
   return dataURL;
