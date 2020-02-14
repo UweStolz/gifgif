e1bcd1e2838f8a73be9e2d53f29e2c1a1addd938
UweStolz <uwe.stolz@prodexa.com>
2020-02-14 12:50:28 +0100

    feat: add tenor endpoint call

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,8 +1,15 @@
-const apiKey = 'IT2EfZQf0HqGmKIlg0Ts9p7akXdKSBl7';
+const giphyApiKey = 'IT2EfZQf0HqGmKIlg0Ts9p7akXdKSBl7';
+const tenorApiKey = 'O4K19202U3YT';
 
-export default async function getTrendingGifsList(): Promise<Giphy.Response> {
-  const response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`);
+export async function getTrendingGifsListFromGiphy(): Promise<Giphy.Response> {
+  const response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${giphyApiKey}`);
   const { data } = await response.json();
   const gifsList: Giphy.Response = data;
   return gifsList;
+}
+
+export async function getTrendingGifsListFromTenor(): Promise<any[]> {
+  const response = await fetch(`https://api.tenor.com/v1/trending?key=${tenorApiKey}?media_filter=minimal?limit=50`);
+  const { data } = await response.json();
+  return data;
 }
