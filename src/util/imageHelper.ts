1f87d63e4c9ab84b083bf6453295a131dc686224
UweStolz <uwe.stolz@protonmail.com>
2020-02-15 17:13:41 +0100

    feat: add (unused) imagehelper function

--- src/util/imageHelper.ts
+++ src/util/imageHelper.ts
@@ -1,10 +1,8 @@
 
-export default function imageToBase64Image(imageElement: HTMLImageElement) {
-  const canvas = document.createElement('canvas');
-  canvas.width = imageElement.width;
-  canvas.height = imageElement.height;
-  // eslint-disable-next-line no-unused-expressions
-  canvas.getContext('2d')?.drawImage(imageElement, 0, 0);
-  const dataURL = canvas.toDataURL('image/webp');
-  return dataURL;
+export default function arrayBufferToImage(arrayBufferView: ArrayBuffer) {
+  const blob = new Blob([arrayBufferView], { type: 'image/webp' });
+  const objectURL = URL.createObjectURL(blob);
+  let imageSource = document.querySelector('img')?.src;
+  imageSource = objectURL;
+  return imageSource;
 }
