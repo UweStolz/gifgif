9ff9513f612325f7433f566a0cb8e6d8805abb5b
UweStolz <uwe.stolz@prodexa.com>
2020-02-20 16:40:53 +0100

    ref: separate panelcontent

---
+++ src/components/PanelContent.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-expansion-panel-content>
+    <v-item-group>
+      <v-container>
+        <v-row
+          justify="start"
+          align="start"
+        >
+          <v-item
+            v-for="(z, index) in contentData.previewUrl[indexValue++].length"
+            :key="z"
+          >
+            <v-img
+              :src="contentData.previewUrl[indexValue][index]"
+              contain
+              max-width="200"
+              max-height="200"
+            />
+          </v-item>
+        </v-row>
+      </v-container>
+    </v-item-group>
+  </v-expansion-panel-content>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class PanelContent extends Vue {
+@Prop({ required: true }) contentData: any
+
+indexValue: number = 0;
+}
+</script>
