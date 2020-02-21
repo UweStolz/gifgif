737e8b66b28a8b28af131ee63f4a038ea6d3dfb8
UweStolz <uwe.stolz@prodexa.com>
2020-02-21 16:25:50 +0100

    feat: add dummy dialog - WIP

---
+++ src/components/Favourites/Dialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-dialog
+    v-model="showDialog"
+    width="500"
+  >
+    <v-card>
+      <v-card-title>Lorem ipsum dolor sit amet</v-card-title>
+      <v-divider />
+      <v-card-actions>
+        <v-btn
+          color="primary"
+          @click="dialog = false"
+        />
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Dialog extends Vue {
+@Prop({ required: true }) showDialog: boolean = false;
+
+dialog: boolean = this.showDialog;
+}
+</script>
--- src/components/Favourites/PanelContent.vue
+++ src/components/Favourites/PanelContent.vue
@@ -1,5 +1,6 @@
 <template>
   <v-expansion-panel-content>
+    <Dialog :show-dialog="dialog" />
     <v-item-group>
       <v-container>
         <v-row
@@ -10,12 +11,23 @@
             v-for="(z, index) in contentData.length"
             :key="z"
           >
-            <v-img
-              :src="contentData[index]"
-              contain
-              max-width="200"
-              max-height="200"
-            />
+            <v-hover
+              v-slot:default="{ hover }"
+            >
+              <v-img
+                :src="contentData[index]"
+                contain
+                max-width="200"
+                max-height="200"
+                @click="dialog = true"
+              >
+                <div
+                  v-if="hover"
+                  class="d-flex"
+                  style="height: 100%; background: rgba(244,67,54,0.3);"
+                />
+              </v-img>
+            </v-hover>
           </v-item>
         </v-row>
       </v-container>
@@ -25,10 +37,17 @@
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator';
+import Dialog from './Dialog.vue';
 
-@Component
+@Component({
+  components: {
+    Dialog,
+  },
+})
 export default class PanelContent extends Vue {
 @Prop({ required: true }) contentData: any
+
+dialog: boolean = false;
 
 indexValue: number = 0;
 }
