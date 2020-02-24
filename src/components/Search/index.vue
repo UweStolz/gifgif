81ff6b224f82e1651e94c9922c38042ffa20f6d4
UweStolz <uwe.stolz@prodexa.com>
2020-02-24 16:54:04 +0100

    feat: add autocomplete search - WIP

--- src/components/Search/index.vue
+++ src/components/Search/index.vue
@@ -1,13 +1,69 @@
 <template>
-  <div>
-    <p>Test</p>
-  </div>
+  <v-container
+    fluid
+  >
+    <v-row
+      align="start"
+      justify="center"
+    >
+      <v-card
+        width="50%"
+        flat
+        tile
+      >
+        <v-autocomplete
+          :v-model="selectedTerms"
+          :search-input.sync="searchQuery"
+          :loading="isLoading"
+          :prepend-icon="icons.mdiImageSearch"
+          :items="trendingTerms"
+          label="GIF search"
+          placeholder="Enter or select search tags"
+          cache-items
+          autofocus
+          hide-no-data
+          deletable-chips
+          chips
+          multiple
+          clearable
+        />
+        <v-card-text>{{ searchResults }}</v-card-text>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component, Watch } from 'vue-property-decorator';
+import { mdiImageSearch } from '@mdi/js';
+import { getSearchGifsFromGiphy, getTrendingSearchTermsFromTenor } from '@/request';
 
 @Component
 export default class Search extends Vue {
+  @Watch('searchQuery')
+  async search(query: string) {
+    this.isLoading = true;
+    const response = await getSearchGifsFromGiphy(query);
+    this.searchResults = response;
+    this.isLoading = false;
+  }
+
+  async mounted() {
+    this.trendingTerms = await getTrendingSearchTermsFromTenor();
+  }
+
+  icons = {
+    mdiImageSearch,
+  }
+
+  trendingTerms: string[] = [];
+
+  searchResults: null|any = null;
+
+  searchQuery: null|string = null;
+
+  isLoading: boolean = false;
+
+  selectedTerms: null|any = null;
 }
 </script>
