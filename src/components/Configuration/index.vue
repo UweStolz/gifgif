53010a255d61059acbb0523d960dbd764c6a5d70
UweStolz <uwe.stolz@prodexa.com>
2020-02-25 14:40:04 +0100

    feat: add sample table

--- src/components/Configuration/index.vue
+++ src/components/Configuration/index.vue
@@ -1,7 +1,64 @@
 <template>
-  <div>
-    <p>I am a Config Page</p>
-  </div>
+  <v-container
+    fluid
+  >
+    <v-row
+      align="start"
+      justify="center"
+    >
+      <v-col cols="auto">
+        <v-avatar
+          size="100"
+          color="#e3e3e3"
+          style="border-radius: 60px;"
+        >
+          <v-icon size="80">
+            {{ icons.mdiCogs }}
+          </v-icon>
+        </v-avatar>
+      </v-col>
+    </v-row>
+
+    <v-row
+      justify="center"
+      align="start"
+    >
+      <v-card
+        width="80%"
+        flat
+        tile
+      >
+        <v-col cols="auto">
+          <v-simple-table
+            fixed-header
+            height="100%"
+          >
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th class="text-left">
+                    Name
+                  </th>
+                  <th class="text-left">
+                    Calories
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="item in desserts"
+                  :key="item.name"
+                >
+                  <td>{{ item.name }}</td>
+                  <td>{{ item.calories }}</td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </v-col>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -11,5 +68,40 @@ import { mdiCogs } from '@mdi/js';
 @Component
 export default class Configuration extends Vue {
 icons = { mdiCogs }
+
+desserts = [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237,
+  },
+  {
+    name: 'Eclair',
+    calories: 262,
+  },
+  {
+    name: 'Cupcake',
+    calories: 305,
+  },
+  {
+    name: 'Gingerbread',
+    calories: 356,
+  },
+  {
+    name: 'Jelly bean',
+    calories: 375,
+  },
+  {
+    name: 'Lollipop',
+    calories: 392,
+  },
+  {
+    name: 'Honeycomb',
+    calories: 408,
+  },
+]
 }
 </script>
