9310321f6263b60db4bd80c9e3448947e79fb9c8
UweStolz <uwe.stolz@prodexa.com>
2020-03-09 16:45:20 +0100

    test: adjust test - WIP

--- tests/unit/components/Navigation/Toolbar.spec.ts
+++ tests/unit/components/Navigation/Toolbar.spec.ts
@@ -1,9 +1,23 @@
 import Toolbar from '@/components/Navigation/Toolbar.vue';
-import shallow from '../../../helper';
+import shallow, { Vue, deepMount } from '../../../helper';
 
 describe('Toolbar.vue', () => {
   it('renders properly', () => {
     const wrapper = shallow(Toolbar);
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it.skip('changes the GifMode', async () => {
+    const wrapper = deepMount(Toolbar);
+    const button = wrapper.get('#tb-inactive-button'); // .find({ name: 'v-btn' }).find('#tb-inactive-button');
+    wrapper.vm.$store.subscribe((mutation, state) => {
+      console.log(JSON.stringify(mutation, undefined, 2));
+      console.log(JSON.stringify(state, undefined, 2));
+    });
+
+    button.trigger('click');
+    await Vue.nextTick();
+    const gifModeState = wrapper.vm.$store.state.gifMode;
+    expect(gifModeState).toBe('random');
   });
 });
