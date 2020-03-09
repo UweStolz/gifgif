e351a6073980a60ac1c92e71105eb492be0cf953
UweStolz <uwe.stolz@protonmail.com>
2020-03-09 20:07:57 +0100

    test: fix test for mode switch

--- tests/unit/components/Navigation/Toolbar.spec.ts
+++ tests/unit/components/Navigation/Toolbar.spec.ts
@@ -7,17 +7,15 @@ describe('Toolbar.vue', () => {
     expect(wrapper.element).toMatchSnapshot();
   });
 
-  it.skip('changes the GifMode', async () => {
+  it('changes the state of gifMode', async () => {
     const wrapper = deepMount(Toolbar);
-    const button = wrapper.get('#tb-inactive-button'); // .find({ name: 'v-btn' }).find('#tb-inactive-button');
+    const button = wrapper.get('#tb-inactive-button');
+    let gifModeState: string = '';
     wrapper.vm.$store.subscribe((mutation, state) => {
-      console.log(JSON.stringify(mutation, undefined, 2));
-      console.log(JSON.stringify(state, undefined, 2));
+      gifModeState = mutation.payload;
     });
-
     button.trigger('click');
     await Vue.nextTick();
-    const gifModeState = wrapper.vm.$store.state.gifMode;
     expect(gifModeState).toBe('random');
   });
 });
