7eac63eef429a79d34bf70828edcc06237304db5
UweStolz <uwe.stolz@prodexa.com>
2020-03-09 13:52:51 +0100

    test: adjust helper

--- tests/helper.ts
+++ tests/helper.ts
@@ -1,5 +1,5 @@
 import {
-  createLocalVue, shallowMount, VueClass, ThisTypedShallowMountOptions,
+  createLocalVue, shallowMount, mount, VueClass, ThisTypedShallowMountOptions, ThisTypedMountOptions, RouterLinkStub,
 } from '@vue/test-utils';
 import VueRouter from 'vue-router';
 import Vue from 'vue';
@@ -8,11 +8,16 @@ import routes from '@/router/routes';
 import fetchMock, { enableFetchMocks } from 'jest-fetch-mock';
 import store from './vuexHelper';
 
+jest.mock('idb');
 enableFetchMocks();
 
 Vue.use(Vuetify);
 const localVue = createLocalVue();
-const vuetify = new Vuetify();
+const vuetify = new Vuetify({
+  icons: {
+    iconfont: 'mdiSvg',
+  },
+});
 const router = new VueRouter({ routes });
 
 export default function shallow(component: VueClass<Vue>, ...args: (ThisTypedShallowMountOptions<Vue> | undefined)[]) {
@@ -26,4 +31,18 @@ export default function shallow(component: VueClass<Vue>, ...args: (ThisTypedSha
   return shallowMount(component, options);
 }
 
-export { fetchMock };
+export function deepMount(component: VueClass<Vue>, ...args: (ThisTypedMountOptions<Vue> | undefined)[]) {
+  const options: ThisTypedShallowMountOptions<Vue> | undefined = {
+    localVue,
+    vuetify,
+    router,
+    store,
+    stubs: {
+      RouterLink: RouterLinkStub,
+    },
+  };
+  Object.assign(options, ...args);
+  return mount(component, options);
+}
+
+export { fetchMock, Vue };
--- tests/vuexHelper.ts
+++ tests/vuexHelper.ts
@@ -1,10 +1,25 @@
-import Vuex from 'vuex';
+import Vuex, { MutationTree, ActionTree } from 'vuex';
 import Vue from 'vue';
 import { States } from '@/store/states';
 
 Vue.use(Vuex);
 
-const actions = {
+const state: States = {
+  gifCount: 0,
+  gifMode: 'trending',
+  showMobileDrawer: false,
+  fullImageMode: false,
+};
+
+const mutations: MutationTree<States> = {
+  initializeStore: jest.fn(),
+  setGifCount: jest.fn(),
+  setGifMode: jest.fn(),
+  setFullImageMode: jest.fn(),
+  setShowMobileDrawer: jest.fn(),
+};
+
+const actions: ActionTree<States, States> = {
   getGifData: jest.fn(),
   setGifData: jest.fn(),
   removeGifData: jest.fn(),
@@ -15,6 +30,8 @@ const actions = {
 };
 
 const store = new Vuex.Store<States>({
+  state,
+  mutations,
   actions,
 });
 
