284b5a73e1bdf504800d85f1fca6c14112e0e54b
UweStolz <uwe.stolz@prodexa.com>
2020-03-11 11:59:54 +0100

    fix: download of gif not possible

--- src/components/shared/CardActions.vue
+++ src/components/shared/CardActions.vue
@@ -67,85 +67,85 @@ import { saveAs } from 'file-saver';
 
 @Component
 export default class CardActions extends Vue {
-@PropSync('previewImageData') syncPreviewImageData!: Blob|string;
+  @PropSync('previewImageData') syncPreviewImageData!: Blob | string;
 
-@PropSync('imageData', { required: true }) syncImageData!: Blob|string;
+  @PropSync('imageData', { required: true }) syncImageData!: Blob | string;
 
-@PropSync('imageId', { required: true }) syncImageId!: string;
+  @PropSync('imageId', { required: true }) syncImageId!: string;
 
-@PropSync('rating', { default: -1, type: Number }) syncRrating!: number;
+  @PropSync('rating', { default: -1, type: Number }) syncRrating!: number;
 
-@Prop({ default: false, type: Boolean }) showRating!: boolean;
+  @Prop({ default: false, type: Boolean }) showRating!: boolean;
 
-@Prop({ default: false, type: Boolean }) isFrontpage!: boolean;
+  @Prop({ default: false, type: Boolean }) isFrontpage!: boolean;
 
-@Prop({ type: Function }) syncFunction!: Function;
+  @Prop({ type: Function }) syncFunction!: Function;
 
-@Emit('delete')
-deleteGifData() {
-  return this.propImageId;
-}
-
-@Watch('syncImageId', { immediate: true })
-setId() {
-  if (this.syncImageId) {
-    this.propImageId = this.syncImageId.startsWith('ggid') ? this.syncImageId : `ggid-${this.syncImageId}`;
+  @Emit('delete')
+  deleteGifData() {
+    return this.propImageId;
   }
-}
 
-@Watch('syncImageData')
-setImageData() {
-  this.propImageData = this.syncImageData;
-}
-
-@Watch('syncPreviewImageData')
-setPreviewImageData() {
-  this.propPreviewImageData = this.syncPreviewImageData;
-}
-
-    icons = {
-      mdiClose,
-      mdiDownload,
-      mdiHeartOutline,
-      mdiHeart,
-      mdiHeartHalfFull,
-      mdiTrashCan,
-      mdiSync,
+  @Watch('syncImageId', { immediate: true })
+  setId() {
+    if (this.syncImageId) {
+      this.propImageId = this.syncImageId.startsWith('ggid') ? this.syncImageId : `ggid-${this.syncImageId}`;
     }
+  }
 
-    propImageId: string = '';
+  @Watch('syncImageData', { immediate: true })
+  setImageData() {
+    this.propImageData = this.syncImageData;
+  }
 
-    propImageData = this.syncImageData;
+  @Watch('syncPreviewImageData', { immediate: true })
+  setPreviewImageData() {
+    this.propPreviewImageData = this.syncPreviewImageData;
+  }
 
-    propPreviewImageData = this.syncPreviewImageData;
+  icons = {
+    mdiClose,
+    mdiDownload,
+    mdiHeartOutline,
+    mdiHeart,
+    mdiHeartHalfFull,
+    mdiTrashCan,
+    mdiSync,
+  }
 
-    saveImage(): void {
-      saveAs(this.propImageData, this.propImageId);
+  propImageId: string = '';
+
+  propImageData = this.syncImageData;
+
+  propPreviewImageData = this.syncPreviewImageData;
+
+  async saveImage(): Promise<void> {
+    saveAs(this.propImageData, this.propImageId);
+  }
+
+  async removeGif(): Promise<void> {
+    await this.$store.dispatch('removeGifData', this.propImageId);
+    this.syncRrating = 0;
+    this.deleteGifData();
+  }
+
+  async updateGifRating(rating: number): Promise<void> {
+    if (this.$store.state.fullImageMode) {
+      await this.$store.dispatch('setGifData', {
+        rating,
+        key: this.propImageId,
+        image: await getBlob(this.propImageData as string),
+        preview: await getBlob(this.propPreviewImageData as string),
+      });
+    } else {
+      await this.$store.dispatch('setGifData', {
+        rating,
+        key: this.propImageId,
+        image: this.propImageData,
+        preview: this.propPreviewImageData,
+      });
     }
-
-    async removeGif(): Promise<void> {
-      await this.$store.dispatch('removeGifData', this.propImageId);
-      this.syncRrating = 0;
-      this.deleteGifData();
-    }
-
-    async updateGifRating(rating: number): Promise<void> {
-      if (this.$store.state.fullImageMode) {
-        await this.$store.dispatch('setGifData', {
-          rating,
-          key: this.propImageId,
-          image: await getBlob(this.propImageData as string),
-          preview: await getBlob(this.propPreviewImageData as string),
-        });
-      } else {
-        await this.$store.dispatch('setGifData', {
-          rating,
-          key: this.propImageId,
-          image: this.propImageData,
-          preview: this.propPreviewImageData,
-        });
-      }
-    }
+  }
 }
 </script>
 
