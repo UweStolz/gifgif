277fa94c29d9d4e37580031b18eba1d753d7ae2d
UweStolz <uwe.stolz@protonmail.com>
2020-03-12 20:54:07 +0100

    test: add tests for fav dialog

--- tests/unit/components/Favourites/Dialog.spec.ts
+++ tests/unit/components/Favourites/Dialog.spec.ts
@@ -1,4 +1,6 @@
 import Dialog from '@/components/Favourites/Dialog.vue';
+import CardActions from '@/components/shared/CardActions.vue';
+import * as blobToImage from '@/util/imageHelper';
 import shallow from '../../../helper';
 
 describe('Dialog.vue', () => {
@@ -12,5 +14,39 @@ describe('Dialog.vue', () => {
       },
     });
     expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('Input images get converted in watch', async () => {
+    const mockImageData = 'someImageData';
+    jest.spyOn(blobToImage, 'default').mockImplementation(() => mockImageData);
+    const wrapper = shallow(Dialog, {
+      propsData: {
+        showDialog: true,
+        fullImageData: 'http://someFullImage.webp',
+        previewImageData: 'http://somePreviewImage.webp',
+        imageId: 'ggid-123321',
+      },
+    });
+    wrapper.setProps({ fullImageData: new Blob(undefined, { type: 'image/webp' }) });
+    await wrapper.vm.$nextTick();
+    expect(wrapper.vm.$data.imageData).toBe(mockImageData);
+  });
+
+  it('Event gets re-emitted', async () => {
+    const wrapper = shallow(Dialog, {
+      stubs: {
+        'card-actions': CardActions,
+      },
+      propsData: {
+        showDialog: true,
+        fullImageData: 'http://someFullImage.webp',
+        previewImageData: 'http://somePreviewImage.webp',
+        imageId: 'ggid-123321',
+      },
+    });
+    const cardActions = wrapper.find(CardActions);
+    cardActions.vm.$emit('delete', { payload: 'gg-someId123' });
+    await wrapper.vm.$nextTick();
+    expect(wrapper).toHaveEmitted('delete');
   });
 });
