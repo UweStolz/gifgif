644df2beaedc10c613749363096afa7b653dcc57
UweStolz <uwe.stolz@protonmail.com>
2020-04-21 19:03:38 +0200

    fix: wrong parameter

--- tests/unit/components/Configuration/zip.spec.ts
+++ tests/unit/components/Configuration/zip.spec.ts
@@ -4,7 +4,7 @@ describe('Zip', () => {
   test('Generates a zip file', async () => {
     zip.createInstance();
     zip.addFileToZip('testFolder', 'testFile', new Blob());
-    const zipResult: string = await zip.generateZip({ type: 'string' });
+    const zipResult: string = await zip.generateZip({ type: 'text' });
     const searchResult = zipResult.match('testFolder/testFile') as RegExpMatchArray;
     expect(searchResult[0]).toBe('testFolder/testFile');
   });
